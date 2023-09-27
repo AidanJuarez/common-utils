84171a1ca9b8b41c82d2036b4b9f431c99809646
lidanyang <lancelot2014@foxmail.com>
2023-09-27 12:10:41 +0800

    feat: fix bug

--- src/entity/index.ts
+++ src/entity/index.ts
@@ -3,6 +3,7 @@ export * from './chain_info';
 export * from './common';
 export * from './order/user_method_sniper_order';
 export * from './token/token';
+export * from './token/token_security_info';
 export * from './user/user_setting';
 export * from './user/user_wallet';
 export * from './user/user_wallet_token_balance';
